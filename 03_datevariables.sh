5aa27d43284e1cd9afa679c84d11e2e7db986700
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 15:42:38 +0530

    add echo command

--- 03_datevariables.sh
+++ 03_datevariables.sh
@@ -3,10 +3,12 @@
 #date=$(date)
 
 start_time=$(date +%s)
+echo "script started executing"
 
 sleep 20
 
 end_time=$(date +%s)
+echo "script exection completed"
 
 total_time=$((end_time-start_time))
 
