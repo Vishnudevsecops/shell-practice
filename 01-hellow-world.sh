9c8e52e7bc6fc7d03688100f66e9472f19718926
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 13:02:14 +0530

    script updated with wishing using variables

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -3,5 +3,5 @@
 person1=$1
 person2=$2
 
-echo "Hello $person1 welcome to our place"
-echo "Hello $person2 welcome to our place"
+echo "Hello $person1:: welcome to our place"
+echo "Hello $person2:: welcome to our place"
