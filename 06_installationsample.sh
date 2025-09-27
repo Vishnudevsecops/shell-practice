72c5042b19006b3d597acfb04d83d72ca13dbbba
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 21:20:20 +0530

    conditions file udpated

--- 06_installationsample.sh
+++ 06_installationsample.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 #check user current user has root access
 user_rootaccess=$(id -u)
 
@@ -12,20 +17,35 @@ fi
 validate(){
 
     if [$1 -ne 0 ]; then
-        echo "error: $2 installation is failed"
+        echo -e "error: $2 installation is $R failed $N"
         exit 1
     else    
-        echo "$2 installation completed"
+        echo -e "$2 installation $G completed $N"
     fi
 }
-dnf install mysql -y
 
-validate $? "mysql" 
 
-dnf install nginix -y
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    validate $? "mysql" 
+else
+    echo "$? is already exist...... $Y Skipped $N"
+fi
 
-validate $? "niginix"
 
-dnf install python3 -y
+dnf list installed nginix
+if [ $? -ne 0 ]; then
+    dnf install nginix -y
+    validate $? "nginix" 
+else
+    echo "$? is already exist...... $Y Skipped $N"
+fi
 
-validate $? "python3"
+dnf list installed phython3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    validate $? "python3" 
+else
+    echo "$? is already exist...... $Y Skipped $N"
+fi
