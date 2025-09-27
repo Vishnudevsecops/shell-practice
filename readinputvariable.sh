18a7d3f54d61f2659f36553a70015f444c5689ac
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 13:33:54 +0530

    script updated with wishing using variables

--- readinputvariable.sh
+++ readinputvariable.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "please enter PIN_Number" #prompt for user
-read PIN_Number #here pin number is variable
-echo "Pin number entered is $PIN_Number" #displays entered pin number by user
+# Prompt the user to enter their PIN silently
+read -s -p "Please enter your PIN: " pin
+echo  # Print a newline after input
 
+# Get the length of the PIN
+pin_length=${#pin}
+
+# If PIN is 3 or fewer characters, don't mask anything
+if [ "$pin_length" -le 3 ]; then
+    echo "PIN: $pin"
+else
+    # Extract last 3 digits
+    last3="${pin: -3}"
+
+    # Create a mask for the first (length - 3) characters
+    mask=$(printf '%*s' $((pin_length - 3)) '' | tr ' ' '*')
+
+    # Show masked PIN
+    echo "PIN: ${mask}${last3}"
+fi
