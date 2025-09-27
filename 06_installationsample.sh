44def5a20da726f46a3bba32172d44a6a9cce51d
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 20:46:02 +0530

    conditions file udpated

--- 06_installationsample.sh
+++ 06_installationsample.sh
@@ -5,7 +5,7 @@ user_rootaccess=$(id -u)
 
 #if use donot have root acces
 if [ $user_rootaccess -ne 0 ]; then
-    echo "Please run the script using root access"
+    echo "Error:: Please run the script using root access"
     exit 1
 fi
 
