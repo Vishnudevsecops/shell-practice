a3fd43705f958d01cecf4dc58f7ae8b5f561f01f
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 13:23:25 +0530

    script updated with wishing using variables

---
+++ readinputvariable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "please enter PIN_Number" #prompt for user
+read PIN_Number #here pin number is variable
+echo "Pin number entered is $PIN_Number" #displays entered pin number by user
+
