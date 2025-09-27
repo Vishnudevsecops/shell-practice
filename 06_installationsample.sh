c187d5ea3fd472bd0a5d37dd4a1c1fb4da90bc5e
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 20:43:01 +0530

    conditions file udpated

---
+++ 06_installationsample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#check user current user has root access
+user_rootaccess=$(id -u)
+
+#if use donot have root acces
+if [ $user_rootaccess -nq 0 ]; then
+    echo "Please run the script using root access"
+    exit 1
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0 ]; then
+    echo "error: Mysql installation is failed"
+    exit 1
+else    
+    echo "mysql installation completed"
+fi
