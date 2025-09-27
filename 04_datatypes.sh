85b93daab939d11dc0888ce34e15af9792246ffb
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-27 16:21:29 +0530

    datat types file updated

--- 04_datatypes.sh
+++ 04_datatypes.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-number1=100
-number2=200
+#number1=100
+#number2=200
 
-sum=$((number1+number2))
+#sum=$((number1+number2))
 
-echo "sum of number1 and number 2 is: $sum"
+#echo "sum of number1 and number 2 is: $sum"
+
+
+leaders=("modi" "pawankalyan" "chandrababunaidu" "kcr" "KTR" "revanthreddy")
+
+echo "list of all leader: ${leaders[@]}"
+echo "first name is: ${leaders[1]}"
