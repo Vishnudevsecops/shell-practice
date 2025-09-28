e8e31e1f898b45f45bfd66df88f996ea1146c0bb
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 12:06:17 +0530

    conditions file udpated

--- 07.loops.sh
+++ 07.loops.sh
@@ -7,6 +7,15 @@ N="\e[0m"
 
 #check if current user has root access
 user_rootaccess=$(id -u)
+
+#create folder in log folder 
+logs_folder="/var/log/shell-script"
+#removing .sh from script name to create a log file
+script_name=$( echo $0 | cut -d "." -f1 )
+log_file="$logs_folder/$script_name.log" 
+
+mkdir -p $logs_folder
+echo "script started excuting at : $(date)" | tee -a $log_file
 #if user does not have root access
 if [ $user_rootaccess -ne 0 ]; then 
     echo "Error:: Please run the script using root access"
@@ -15,29 +24,29 @@ fi
 
 validate(){
     if [ $1 -ne 0 ]; then
-        echo -e "error:: $2 installation is $R failed $N"
+        echo -e "error:: $2 installation is $R failed $N" | tee -a $log_file
         exit 1
     else
-        echo -e "$2 installation $G Completed $N"
+        echo -e "$2 installation $G Completed $N" | tee -a $log_file
     fi
 
 }
 if [ $# -eq 0 ]; then
-    echo "Error:: Please provide package names to install"
+    echo "Error:: Please provide package names to install" | tee -a $log_file
     exit 1
 fi 
 for package in $@; do
-    dnf list installed $package 
+    dnf list installed $package &>>$log_file
     if [ $? -ne 0 ]; then
         dnf install $package -y
         validate $? "$package"
         if [ $? -ne 0]; then
-            echo -e "error:: $package installation is $R failed $N"
+            echo -e "error:: $package installation is $R failed $N" | tee -a $log_file
             exit 1
         else
-            echo -e "$package installation $G completed $N"
+            echo -e "$package installation $G completed $N" | tee -a $log_file
     fi
         else
-            echo -e "$package is already exist......$Y Skipped $N"
+            echo -e "$package is already exist......$Y Skipped $N" | tee -a $log_file
         fi
     done
