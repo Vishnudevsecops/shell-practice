f8f6c8f9c3ee6b4b8c3df0e012d2872aa96f963f
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 11:43:06 +0530

    loops file created

---
+++ 07.loops.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check if current user has root access
+user_rootaccess=$(id -u)
+#if user does not have root access
+if [ $user_rootacess -ne 0 ]; then 
+    echo "Error:: Please run the script using root access"
+    exit 1
+fi
+
+for package in $@
+
+do
+    dnf list installed $package 
+    if [ $? -ne 0 ]; then
+        dnf install $package -y
+        if [ $? -ne 0]; then
+            echo -e "error:: $package installation is $R failed $N"
+            exit 1
+            else
+                echo -e "$package installation $G completed $N"
+                fi
+                else
+                    echo -e "$package is already exist......$Y Skipped $N"
+                    fi
+                    done
+                ]]
